fddf0878636e7a3f1cbde787a90799f380ce5556
amosmoyo <amosmoyo5300@gmail.com>
2019-10-13 13:24:20 +0300

    message change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,FormsModule
